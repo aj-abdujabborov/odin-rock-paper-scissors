e8b22e833e58662baa70471708ef9070cb9fe518
aj-abdujabborov <60819490+aj-abdujabborov@users.noreply.github.com>
2023-05-29 01:14:40 +0500

    Make the JavaScript code self-documenting

--- script.js
+++ script.js
@@ -1,35 +1,60 @@
-// Global variables
+const players = ["human", "computer"];
+
+const numTools = 3;
 const tools = ["rock", "paper", "scissors"];
-const callbacks = [null, null, null];
 const toolImgs = {
     rock: "./imgs/rock.jpeg",
     paper: "./imgs/paper.jpeg",
     scissors: "./imgs/scissors.jpeg"
 }
+const toolClickCallbacks = [null, null, null];
+
 const winScore = 3;
-const numTools = 3;
 const playerScores = {
     human: 0,
     computer: 0
 }
-const winToolShadow = "0 0 30px 8px rgba(255, 255, 255, 0.5)";
+
+const defaultToolShadow = document.querySelector(`.tool-container`).style.boxShadow;
+const winChoiceShadow = "0 0 30px 8px rgba(255, 255, 255, 0.5)";
+const defaultPlayerShadow = document.querySelector(`.profile-container`).style.boxShadow;
 const winPlayerShadow = "0 0 100px 50px rgba(255, 255, 255, 0.4)";
-const times = { // timing (t=0 when human gives input)
+
+const msAfterChoice = {
     human: {
-        showToolDelay: 0
+        choiceVisible: 0
     },
     computer: {
-        showToolDelay: 0.5 * 1000
+        choiceVisible: 0.5 * 1000
     },
-    showToolWinner: 0.8 * 1000,
-    updateStars: 1 * 1000, // how long before winning star is shown
-    hideToolDelay: 2 * 1000, // how long before player can input again
-    gameRestartTime: 5 * 1000
+    winChoiceHighlight: 0.8 * 1000,
+    starVisible: 1 * 1000,
+    winPlayerHighlight: 1 * 1000,
+    choiceHidden: 2 * 1000,
+    nextRound: 2 * 1000,
+    gameOverRestart: 5 * 1000
 }
 
-// Setup
-makeCallbackFunctions();
+makeToolClickCallbackFunctions();
 setAcceptInputs(true);
+
+function makeToolClickCallbackFunctions() {
+    for (let i = 0; i < numTools; i++) {
+        toolClickCallbacks[i] = () => {playRound(tools[i]);};
+    }
+}
+
+function setAcceptInputs(ON) {
+    for (let i = 0; i < numTools; i++) {
+        let target = document.querySelector(`.tools-container > .tool-container.${tools[i]}`);
+        if (ON) {
+            target.addEventListener("click", toolClickCallbacks[i], false);
+        }
+        else {
+            target.removeEventListener("click", toolClickCallbacks[i], false);
+        }
+    }
+}
 
 function playRound(playerSelection) {
     setAcceptInputs(false);
@@ -37,78 +62,37 @@ function playRound(playerSelection) {
     const winner = getWinnerOfRound(playerSelection, computerSelection);
     if (winner !== "tie") {
         ++playerScores[winner];
-        setTimeout(() => {showScore(playerScores[winner], winner)}, times.updateStars);
+        setTimeout(() => {updateStars(playerScores[winner], winner)}, msAfterChoice.starVisible);
     }
     showMove(playerSelection, computerSelection, winner);
 
     if (!isGameOver()) {
-        setTimeout(() => {setAcceptInputs(true)}, times.hideToolDelay);
+        setTimeout(() => {setAcceptInputs(true)}, msAfterChoice.nextRound);
     }
     else {
-        handleGameOver();
+        const profile = document.querySelector(`.player.${winner} .profile-container`);
+        showWinnerPlayer(profile);
+        resetScores();
+        setTimeout(() => {setAcceptInputs(true);}, msAfterChoice.gameOverRestart);
     }
 }
 
-function handleGameOver() {
-    const winPlayer = playerScores.human > playerScores.computer ? "human" : "computer";
-    const winPlayerVis = document.querySelector(`.player.${winPlayer} .profile-container`);
-    const defaultPlayerShadow = winPlayerVis.style.boxShadow;
-    
-    setTimeout(() => {winPlayerVis.style.boxShadow = winPlayerShadow;}, times.updateStars);
-    setTimeout(() => {
-        winPlayerVis.style.boxShadow = defaultPlayerShadow;
-        playerScores.human = 0;
-        playerScores.computer = 0;
-        showScore(playerScores.human, "human");
-        showScore(playerScores.computer, "computer");
-        setAcceptInputs(true);
-    }, times.gameRestartTime);
+function showWinnerPlayer(profile) {
+    setTimeout(() => {profile.style.boxShadow = winPlayerShadow;}, msAfterChoice.winPlayerHighlight);
+    setTimeout(() => {profile.style.boxShadow = defaultPlayerShadow;}, msAfterChoice.gameOverRestart);
 }
 
-function isGameOver() {
-    return Math.max(playerScores.human, playerScores.computer) === winScore;
+function resetScores() {
+    setTimeout(
+        () => {
+            playerScores.human = playerScores.computer = 0;
+            updateStars(0, "human"); 
+            updateStars(0, "computer");}, 
+        msAfterChoice.gameOverRestart
+        );
 }
 
-function getComputerChoice() {
-    const options = ["rock", "paper", "scissors"];
-    const num = Math.floor(Math.random() * 3);
-    return options[num];
-}
-
-function getWinnerOfRound(playerSelection, computerSelection) {
-    // Tie?
-    if (playerSelection === computerSelection) { return "tie"; }
-    // Move combo
-    const combo = (playerSelection + "-" + computerSelection).toLowerCase();
-    // Find winner
-    const winList = ["paper-rock", "scissors-paper", "rock-scissors"];
-    for (const winCombo of winList) {
-        if (combo === winCombo) {
-            return "human";
-        }
-    }
-    return "computer";
-}
-
-function makeCallbackFunctions() {
-    for (let i = 0; i < numTools; i++) {
-        callbacks[i] = () => {playRound(tools[i]);};
-    }
-}
-
-function setAcceptInputs(state) {
-    for (let i = 0; i < numTools; i++) {
-        let target = document.querySelector(`.tools-container > .tool-container.${tools[i]}`);
-        if (state === true) {
-            target.addEventListener("click", callbacks[i], false);
-        }
-        else {
-            target.removeEventListener("click", callbacks[i], false);
-        }
-    }
-}
-
-function showScore(score, player) {
+function updateStars(score, player) {
     for (let i = 1; i <= winScore; i++) {
         let star = document.querySelector(`.star-${i}-${player}`);
         if (i <= score) {
@@ -121,29 +105,39 @@ function showScore(score, player) {
 }
 
 function showMove(humanTool, computerTool, winner) {
-    const players = ["human", "computer"];
-    const tools = {
-        human: humanTool,
-        computer: computerTool
-    }
-
+    const tools = {human: humanTool, computer: computerTool};
     for (const player of players) {
-        // Choose correct tool
-        let currentMoveImg = document.querySelector(`.player.${player} .choice > img`);
-        currentMoveImg.src = toolImgs[tools[player]];
+        let choiceImg = document.querySelector(`.player.${player} .choice > img`);
+        choiceImg.src = toolImgs[tools[player]];
 
-        // Show
-        let currentMove = document.querySelector(`.player.${player} .choice`);
-        let defaultShadow = currentMove.style.boxShadow;
-        setTimeout(() => {currentMove.style.visibility = "visible";}, times[player].showToolDelay);
+        let choice = document.querySelector(`.player.${player} .choice`);
+        setTimeout(() => {choice.style.visibility = "visible";}, msAfterChoice[player].choiceVisible);
+        setTimeout(() => {choice.style.visibility = "hidden";}, msAfterChoice.choiceHidden);
 
-        // Hide again
-        setTimeout(() => {currentMove.style.visibility = "hidden";}, times.hideToolDelay);
-
-        // Show and hide victory shadow
         if (player === winner) {
-            setTimeout(() => {currentMove.style.boxShadow = winToolShadow;}, times.showToolWinner);
-            setTimeout(() => {currentMove.style.boxShadow = defaultShadow;}, times.hideToolDelay);
+            setTimeout(() => {choice.style.boxShadow = winChoiceShadow;}, msAfterChoice.winChoiceHighlight);
+            setTimeout(() => {choice.style.boxShadow = defaultToolShadow;}, msAfterChoice.choiceHidden);
         }
     }
+}
+
+function getComputerChoice() {
+    return tools[Math.floor(Math.random() * numTools)];
+}
+
+function getWinnerOfRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) { return "tie"; }
+    
+    const humanWinCases = ["paper-rock", "scissors-paper", "rock-scissors"];
+    const currentCase = (playerSelection + "-" + computerSelection);
+    for (const humanWinCase of humanWinCases) {
+        if (currentCase === humanWinCase) {
+            return "human";
+        }
+    }
+    return "computer";
+}
+
+function isGameOver() {
+    return Math.max(playerScores.human, playerScores.computer) === winScore;
 }
